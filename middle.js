ec004fa159721a6b4e68c5f9dd31178ba9de44ea
obaidullah Jasim iitp <obaidullahjasim24@gmail.com>
2026-09-22 22:11:40 +0530

    Add files via upload

---
+++ middle.js
@@ -0,0 +1,52 @@
+// const exprees= require('express')
+
+// const app = exprees()
+// const port = 3003
+
+// app.use(exprees.json());
+
+ 
+
+
+
+
+// // middleware login auth validation
+
+// // const loginmiddleware = function (req,res,next){
+// //     console.log("login kr rha hu")
+// //     next()
+// // }
+// // app.use(loginmiddleware)
+
+// // const authmiddleware = function (req,res,next){
+// //     console.log("Auth kr rha hu")
+// //     //res.send("chalo seedha ghr")
+// //     next()
+// // }
+// // app.use(authmiddleware)
+
+// // const validationmiddleware = function(req,res,next){
+// //     console.log("Validation kr rha hu")
+// //     next()
+
+// // }
+// // app.use(validationmiddleware)
+
+// // const rout = require('./routs/rout')
+
+// // app.use('/api',rout)
+
+// app.post('/',(req,res)=>{
+//     console.log("mai route pr hu")
+//     console.log(req.body);
+//     res.send("Hello world!")
+// })
+
+
+
+
+
+// app.listen(port,()=>{
+//     console.log("Server is running on")
+
+// })
